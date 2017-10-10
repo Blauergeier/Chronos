851241f0245c48ab4a807e78e901606ee9421255
Blauergeier <blauer.geier@yahoo.com>
2017-10-10 17:51:25 +0200

    Changed storage format. Now 2 seperate files. Adding of entries now works! Deletion does not.

--- main.py
+++ main.py
@@ -3,25 +3,20 @@ import argparse
 
 debug = True
 
-def save(data):
-    save_file = open('data.txt', 'w+')
+def save_in_file(data, filename):
+    save_file = open(filename, 'w+')
     save_file.write(json.dumps(data))
     save_file.close()
     return
 
-def read_save_file():
-    save_file = open('data.txt','r')
-    dictionary = json.loads(save_file.read())
-    save_file.close()
+def read_save_file(filename):
+    try:
+        save_file = open(filename,'r')
+        dictionary = json.loads(save_file.read())
+        save_file.close()
+    except IOError:
+        dictionary = {}
     return dictionary
-
-def clear_stash():
-    stash.clear()
-    return
-
-def add_subject(key):
-    stash[key] = 0
-    return
 
 #TODO: add Logger
 def custom_print(text):
@@ -32,42 +27,49 @@ def custom_print(text):
 print "[#] Welcome to Chronos!"
 custom_print("[*] Reading from storage.")
 
-stash = read_save_file()
-names = {}
-time = {}
+names = read_save_file('names.txt')
+time = read_save_file('time.txt')
 
 
 #handle commandline input
-parser = argparse.ArgumentParser(description='You can add or remove tasks or time as well as display your current status.')
+parser = argparse.ArgumentParser(description="You can add or remove tasks or time as well as display your current status.")
 parser.add_argument('-d', '--display', action="store_true", help="This option displays your current status.")
 parser.add_argument('-a', '--add', action="store_true", help="Starts the menu for adding tasks")
-parser.add_argument('-r', "--remove", action="store", help="Starts the process of deleting tasks")
+parser.add_argument('-r', "--remove", action="store_true", help="Starts the process of deleting tasks")
 args = parser.parse_args()
 
 if args.display:
-    print json.dumps(stash)
+    print json.dumps(names)
+    print json.dumps(time)
+
 elif args.add:
-    display_name = raw_input("Please specify the full name of the task.")
-    handle = raw_input("Please specify a short handle for the task.")
+    display_name = raw_input("[!] Please specify the full name of the task: ")
+    handle = raw_input("[!] Please specify a short handle for the task: ")
 
     names[handle] = display_name
     time[handle] = 0
-    print "[#] Tasks was added!"
+    print "[#] Task was added!"
 
+#TODO: Deletion does not work
 elif args.remove:
-    user_input = raw_input("Please input the full name or handle of the tasks you want to delete.")
+    user_input = raw_input("[!] Please input the full name or handle of the tasks you want to delete: ")
     for e in names:
         #TODO: Improve code
-        if e.key is user_input:
-            del names[e.key]
+        print "e = %s" % e
+        if e is user_input:
+            print "test1"
+            del names[e]
             break
-        elif e.value is user_input:
-            del names[e.key]
+        elif names[e] is user_input:
+            print "test2"
+            del names[e]
             break
     print "[#] Task was removed!"
-
 
 #TODO: add options for adding and removing tasks as well as time
 
 custom_print("[*] Saving.")
-save(stash)
+save_in_file(names, "names.txt")
+save_in_file(time, "time.txt")
+
+print "[#] Goodbye!"
