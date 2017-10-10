ad855f8bcad369019183147053be4ba3ec943f4c
Blauergeier <blauer.geier@yahoo.com>
2017-10-10 15:31:36 +0200

    Started implementing add/remove/display of taks; not complete

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import json
+import argparse
+
+debug = True
 
 def save(data):
     save_file = open('data.txt', 'w+')
@@ -20,13 +23,51 @@ def add_subject(key):
     stash[key] = 0
     return
 
-print "[*] Reading from storage."
+#TODO: add Logger
+def custom_print(text):
+    if debug:
+        print text
+    return
+
+print "[#] Welcome to Chronos!"
+custom_print("[*] Reading from storage.")
+
 stash = read_save_file()
-
-print json.dumps(stash)
-
-raw_input("")
+names = {}
+time = {}
 
 
-print "[*] Saving."
+#handle commandline input
+parser = argparse.ArgumentParser(description='You can add or remove tasks or time as well as display your current status.')
+parser.add_argument('-d', '--display', action="store_true", help="This option displays your current status.")
+parser.add_argument('-a', '--add', action="store_true", help="Starts the menu for adding tasks")
+parser.add_argument('-r', "--remove", action="store", help="Starts the process of deleting tasks")
+args = parser.parse_args()
+
+if args.display:
+    print json.dumps(stash)
+elif args.add:
+    display_name = raw_input("Please specify the full name of the task.")
+    handle = raw_input("Please specify a short handle for the task.")
+
+    names[handle] = display_name
+    time[handle] = 0
+    print "[#] Tasks was added!"
+
+elif args.remove:
+    user_input = raw_input("Please input the full name or handle of the tasks you want to delete.")
+    for e in names:
+        #TODO: Improve code
+        if e.key is user_input:
+            del names[e.key]
+            break
+        elif e.value is user_input:
+            del names[e.key]
+            break
+    print "[#] Task was removed!"
+
+
+#TODO: add options for adding and removing tasks as well as time
+
+custom_print("[*] Saving.")
 save(stash)
